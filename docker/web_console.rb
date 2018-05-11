9b2175ae5b05b312967ecefa9c20bb07c17ad6f2
Mr. Outis <mroutis@protonmail.com>
2018-05-10 23:59:57 -0500

    Deny web-console settings to run in production

--- docker/web_console.rb
+++ docker/web_console.rb
@@ -1,5 +1,7 @@
 # Settings to support Docker IP bindings
 
+return if Rails.env.production?
+
 Rails.configuration.web_console.whitelisted_ips = '0.0.0.0'
 
 BetterErrors::Middleware.allow_ip! '0.0.0.0/0' if defined?(BetterErrors)
