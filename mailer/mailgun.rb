d215d8fb3e251a049ba2fa9b6c2aa40aed5cc330
Mr. Outis <mroutis@protonmail.com>
2018-06-22 09:20:04 -0500

    Prevent HOST requiring at mailgun initializer

--- mailer/mailgun.rb
+++ mailer/mailgun.rb
@@ -1,9 +1,9 @@
 return unless Rails.env.production?
 
 Rails.configuration.action_mailer.mailgun_settings = {
-  api_key: ENV['MAILGUN_APIKEY'],
+  api_key: ENV['MAILGUN_API_KEY'],
   domain: ENV['MAILGUN_DOMAIN']
 }
 
 Rails.configuration.action_mailer.delivery_method = :mailgun
-Rails.configuration.action_mailer.asset_host = ENV.fetch('HOST')
+Rails.configuration.action_mailer.asset_host = ENV['HOST']
