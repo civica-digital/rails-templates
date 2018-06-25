bd4f3f0fca94d01c70d4bdb65da4027bdca2d7c3
Mr. Outis <mroutis@protonmail.com>
2018-06-25 13:18:42 -0500

    Use ActionMailer instead of Rails config

--- mailer/aws-ses.rb
+++ mailer/aws-ses.rb
@@ -1,10 +1,16 @@
 return unless Rails.env.production?
 
+host = ENV.fetch('HOST') { 'change-me-at-mailer.example.com' }
+access_key = ENV['AWS_SES_ACCESS_KEY'] || ENV['AWS_ACCESS_KEY']
+secret_key = ENV['AWS_SES_SECRET_KEY'] || ENV['AWS_SECRET_KEY']
+region = ENV.fetch('AWS_REGION') { 'us-east-1' }
+
+ActionMailer::Base.asset_host = host
+ActionMailer::Base.default_url_options = { host: host }
+
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  access_key_id: ENV['AWS_SES_ACCESS_KEY'] || ENV['AWS_ACCESS_KEY'],
-  secret_access_key: ENV['AWS_SES_SECRET_KEY'] || ENV['AWS_SECRET_KEY'],
-  server: "email.#{ENV.fetch('AWS_REGION') { 'us-east-1' }}.amazonaws.com"
+  access_key_id: access_key,
+  secret_access_key: secret_key,
+  server: "email.#{region}.amazonaws.com"
 
-Rails.configuration.action_mailer.delivery_method = :ses
-Rails.configuration.action_mailer.asset_host = ENV['HOST']
-
+ActionMailer::Base.delivery_method = :ses
--- mailer/mailgun.rb
+++ mailer/mailgun.rb
@@ -1,9 +1,12 @@
 return unless Rails.env.production?
 
-Rails.configuration.action_mailer.mailgun_settings = {
+host = ENV.fetch('HOST') { 'change-me-at-mailer.example.com' }
+
+ActionMailer::Base.asset_host = host
+ActionMailer::Base.default_url_options = { host: host }
+
+ActionMailer::Base.delivery_method = :mailgun
+ActionMailer::Base.mailgun_settings = {
   api_key: ENV['MAILGUN_API_KEY'],
   domain: ENV['MAILGUN_DOMAIN']
 }
-
-Rails.configuration.action_mailer.delivery_method = :mailgun
-Rails.configuration.action_mailer.asset_host = ENV.fetch('HOST') { '' }
