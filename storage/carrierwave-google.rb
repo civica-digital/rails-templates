b10f30556f4748106da90bb62f6570948722b24b
Mr. Outis <mroutis@protonmail.com>
2018-06-25 11:07:18 -0500

    Add template with carrierwave and google cloud

---
+++ storage/carrierwave-google.rb
@@ -0,0 +1,23 @@
+CarrierWave.configure do |config|
+  config.storage = :file
+  config.ignore_integrity_errors = false
+  config.ignore_processing_errors = false
+  config.ignore_download_errors = false
+
+  return unless Rails.env.production? || ENV.key?('GOOGLE_STORAGE_BUCKET')
+
+  config.ignore_integrity_errors = true
+  config.ignore_processing_errors = true
+  config.ignore_download_errors = true
+
+  config.storage = :fog
+  config.fog_public = false
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.days.to_i}" }
+  config.fog_provider = 'fog/google'
+  config.fog_directory = ENV.fetch('GOOGLE_STORAGE_BUCKET')
+  config.fog_credentials = {
+    provider:                         'Google',
+    google_storage_access_key_id:     ENV.fetch('GOOGLE_STORAGE_ACCESS_KEY'),
+    google_storage_secret_access_key: ENV.fetch('GOOGLE_STORAGE_SECRET_KEY'),
+  }
+end
---
+++ storage/main.rb
@@ -0,0 +1,53 @@
+def download(file, output: nil, &block)
+  output ||= file
+  repo = 'https://raw.githubusercontent.com/civica-digital/rails-templates'
+  branch = 'master'
+  directory = 'storage'
+  url = "#{repo}/#{branch}/#{directory}/#{file}"
+
+  render = open(url) do |input|
+    data = input.binmode.read
+    if block_given? then block.call(data) else data end
+  end
+
+  create_file output, render
+end
+
+def content_in_file?(content, file)
+  File.read(file).include?(content)
+end
+
+def file_encrypted?(filename)
+  `git-crypt status -e`.include?(filename)
+end
+
+def add_env_var(variable, value=nil)
+  environment_file = 'deploy/staging/provisions/environment'
+
+  return unless File.exist?(environment_file)
+
+  say("Error: You don't have git-crypt installed", :red) and
+    return unless system('which git-crypt')
+
+  say("Error: Your environment file is not encrypted", :red) and
+    return unless file_encrypted?(environment_file)
+
+  unless content_in_file?(variable, environment_file)
+    value ||= ask("> #{variable}=", :green)
+    append_to_file environment_file, "#{variable}=#{value}\n"
+  end
+end
+
+if yes?('> Do you want to use Google Cloud Storage?', :green)
+  gem 'carrierwave'
+  gem 'fog-google'
+  gem 'google-api-client', '> 0.8.5', '< 0.9'
+  gem 'mime-types'
+  run 'bundle install'
+
+  download 'carrierwave-google.rb', output: 'config/initializers/carrierwave.rb'
+
+  add_env_var('GOOGLE_STORAGE_BUCKET')
+  add_env_var('GOOGLE_STORAGE_ACCESS_KEY')
+  add_env_var('GOOGLE_STORAGE_SECRET_KEY')
+end
